7281aaf84759bb3b6de68bd5c754c539dda2eff6
Miss. Mayuri Babaso Mane <129718471+Mayuri3166@users.noreply.github.com>
2024-06-03 16:55:56 +0530

    Update Freebook.jsx

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -11,7 +11,7 @@ function Freebook() {
   useEffect(()=>{
     const getBook = async()=>{
       try{
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstoreapp-sigma.vercel.app/book");
         console.log(res.data);
         const data = res.data.filter((data)=>data.category==="FREE")
         setBook(data)
